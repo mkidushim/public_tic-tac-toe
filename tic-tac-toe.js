f0c3f38c3893b2e11c09a37bd675536338b2e438
mkidushim <mkidushim@gmail.com>
2015-05-13 11:41:16 -0700

    Finished tic-tac-toe_.5

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
-            $('html').html('<div class="container">');
-            $('.container > div').append("<div class= row");
-            var score_array = ['', '', '', '', '', '', '', '', ''];
-            var player1 = true;
-            $('#parent').on('click', '.box', function() {
+    console.log('initiating')
+    var score_array = ['', '', '', '', '', '', '', '', ''];
+    var player1 = true;
+    console.log('assigning click handler to', $('#parent > .row'))
+    $('#main_container').on('click', '.box', function() {
 
-                if (player1) {
-                    $(this).addClass(selected1);
-                    console.log("button was clicked");
-                    score_array[$(this).attr('index')] = 'x';
-                    console.log(score_array)
-                    player1 = !player1;
-                } else {
-                    $(this).addClass("selected2");
-                    score_array[$(this).attr('index')] = "o";
-                    player1 = !player1;
-                }
-            });
+        if (player1) {
+            $(this).addClass('selected1');
+            console.log("button was clicked");
+            score_array[$(this).attr('index')] = 'x';
+            console.log(score_array)
+
+            player1 = !player1;
+        } else {
+            $(this).addClass("selected2");
+            score_array[$(this).attr('index')] = "o";
+            player1 = !player1;
         }
+    });
+});

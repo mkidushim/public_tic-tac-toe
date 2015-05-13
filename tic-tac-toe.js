70d73f8c3d9bf87c2422d5abaed941f655359449
Keith Corros <kcorros@gmail.com>
2015-05-13 16:08:50 -0700

    4:08

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,49 +1,62 @@
-
 var score_array = ['', '', '', '', '', '', '', '', ''];
-$(document).ready(function() {
-    console.log('initiating')
-    var player1 = true;
-    console.log('assigning click handler to', $('#parent > .row'))
-    $('#main_container').on('click', '.box', function() {
+$(document).ready(function() 
+{
+            console.log('initiating')
+            var player1 = true;
+            console.log('assigning click handler to', $('#parent > .row'))
+            $('#main_container').on('click', '.box', function() 
+            {
 
 
 
-        if (player1) {
-            $(this).addClass('selected1');
-            console.log("button was clicked");
-            score_array[$(this).attr('index')] = 'g';
-            console.log(score_array);
-            $('#player2').removeClass('border');
-            $('#player1').addClass('border');
-            player1 = false;
-        } else if (!player1) {
-            $(this).addClass("selected2");
-            console.log("button was clicked")
-            score_array[$(this).attr('index')] = "o";
-            $('#player1').removeClass('border');
-            $('#player2').addClass('border');
-            player1 = true;
-        }
-    });
+                if (player1) 
+                {
+                    $(this).addClass('selected1');
+                    console.log("button was clicked");
+                    score_array[$(this).attr('index')] = 'g';
+                    console.log(score_array);
+                    $('#player2').removeClass('border');
+                    $('#player1').addClass('border');
+                    player1 = false;
+                    for (var i = 0; i < win_conditions.length; i++) 
+                    {
+                        if (score_array[win_conditions[i][0]] == score_array[win_conditions[i][1]] && score_array[win_conditions[i][1]] == score_array[win_conditions[i][2]]) 
+                        {
+                            console.log('you win!')
+                        }
+                    }
+                }else if (!player1)
+                    	{
+                        $(this).addClass("selected2");
+                        console.log("button was clicked")
+                        score_array[$(this).attr('index')] = "o";
+                        $('#player1').removeClass('border');
+                        $('#player2').addClass('border');
+                        player1 = true;
+                        for (var i = 0; i < win_conditions.length; i++)
+                        {
+                            if (score_array[win_conditions[i][0]] == score_array[win_conditions[i][1]] && score_array[win_conditions[i][1]] == score_array[win_conditions[i][2]]) 
+                        	{
+                                console.log('you win!')
+                        	}
+                        }   
+                		}
+            });
 });
-var win_conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 3, 6],
-    [0, 4, 8],
-    [6, 4, 2]
-];
+            var win_conditions = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 3, 6],
+                [0, 4, 8],
+                [6, 4, 2]
+            ];
 
 
-for (var i = 0; i < win_conditions.length; i++) {
-    if (score_array[win_conditions[i][0]] == score_array[win_conditions[i][1]] && score_array[win_conditions[i][1]] == score_array[win_conditions[i][2]]) {
-        console.log('you win!')
-    }
 
 
-}
 
+        
 

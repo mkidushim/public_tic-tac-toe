cbdcb72c077548264786aad88f5e782160fc0f16
mkidushim <mkidushim@gmail.com>
2015-05-13 09:35:30 -0700

    Added dynamic game board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+            $('html').html('<div class="container">');
+            $('.container > div').append("<div class= row");
+            var score_array = ['', '', '', '', '', '', '', '', ''];
+            var player1 = true;
+            $('#parent').on('click', '.box', function() {
 
+                if (player1) {
+                    $(this).addClass(selected1);
+                    console.log("button was clicked");
+                    score_array[$(this).attr('index')] = 'x';
+                    console.log(score_array)
+                    player1 = !player1;
+                } else {
+                    $(this).addClass("selected2");
+                    score_array[$(this).attr('index')] = "o";
+                    player1 = !player1;
+                }
+            });
+        }

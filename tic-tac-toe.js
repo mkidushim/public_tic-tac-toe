6e033185db77de3f2f9c9b4b5d2999ac09cbe5ae
mkidushim <mkidushim@gmail.com>
2015-05-13 12:28:33 -0700

    started 1.0

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,6 @@
+var score_array = ['', '', '', '', '', '', '', '', ''];
 $(document).ready(function() {
     console.log('initiating')
-    var score_array = ['', '', '', '', '', '', '', '', ''];
     var player1 = true;
     console.log('assigning click handler to', $('#parent > .row'))
     $('#main_container').on('click', '.box', function() {
@@ -14,8 +14,27 @@ $(document).ready(function() {
             player1 = !player1;
         } else {
             $(this).addClass("selected2");
+            console.log("button was clicked")
             score_array[$(this).attr('index')] = "o";
             player1 = !player1;
         }
     });
 });
+var win_conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 3, 6],
+    [0, 4, 8],
+    [6, 4, 2]
+];
+
+for (var i = 0; i < win_conditions.length; i++) {
+    if (score_array[win_conditions[i][0]] == score_array[win_conditions[i][1]] && score_array[win_conditions[i][1]] == score_array[win_conditions[i][2]]) {
+        console.log('you win!')
+    }
+
+
+}

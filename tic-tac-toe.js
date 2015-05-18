9243b0611a6d1c9d3bb379a42a8b9e48dbfe9e52
mkidushim <mkidushim@gmail.com>
2015-05-17 17:28:23 -0700

    Finished version 1.0

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -37,7 +37,7 @@ $(document).ready(function()
                     $('.modal-fade').append("<div class='modal-dialog'>");
                     $('.modal-dialog').append("<div class='modal-content'>");
                     $(".modal-content").append('<div class="modal-header">');
-                    $('.modal-header').append('<div class="modal-body">Thank you for playing Keith and Mike\'s Tic-Tac-Toe, Player 1 Wins!</div>');
+                    $('.modal-header').append('<div class="modal-body">Thank you for playing Keith and Mike\'s Tic-Tac-Toe, Player \'O\' Wins!</div>');
                     $('.modal-content').append('<div class="modal-footer">');
                     $('.modal-footer').append("<button class='reset' onclick='reset();make_board;'>Reset</button>")
                 }
@@ -57,7 +57,7 @@ $(document).ready(function()
                     $('.modal-fade').append("<div class='modal-dialog'>");
                     $('.modal-dialog').append("<div class='modal-content'>");
                     $(".modal-content").append('<div class="modal-header">');
-                    $('.modal-header').append('<div class="modal-body">Thank you for playing Keith and Mike\'s Tic-Tac-Toe, Player 2 Wins!</div>');
+                    $('.modal-header').append('<div class="modal-body">Thank you for playing Keith and Mike\'s Tic-Tac-Toe, Player \'X\' Wins!</div>');
                     $('.modal-content').append('<div class="modal-footer">');
                     $('.modal-footer').append("<button class='reset' onclick='reset();make_board;'>Reset</button>")
                 }
@@ -79,8 +79,8 @@ function initialize_game_board() {
         $('.row.row_2').append("<div index='3' class='box col-md-4'></div>").append("<div index='4' class='box col-md-4'></div>").append("<div index='5' class='box col-md-4'></div>");
         $('#parent').append("<div class='row row_3'>");
         $('.row.row_3').append("<div index='6' class='box col-md-4'></div>").append("<div index='7' class='box col-md-4'></div>").append("<div index='8' class='box col-md-4'></div>");
-        $('#box_container').append('<div class="player_info col-md-3"></div>');
-        $('.player_info').append('<div id="player1">Player Orange</div>').append('<div id="player2">Player Green</div>');
+        $('#box_container').append('<div class="player_info col-md-4"></div>');
+        $('.player_info').append('<div id="player1">Player \'O\'</div>').append('<div id="player2">Player \'X\'</div>');
 
         click = false;
     }
